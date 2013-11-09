f3906a9d0bd60dd205fb9127e1f838a1fdacd401
cdueltgen <cdueltgen@gmail.com>
2013-11-08 22:43:28 -0800

    changed to not use password because that was some bs right there.

--- points.py
+++ points.py
@@ -1,14 +1,6 @@
-import gdata
 import gspread
-import secret
-
-# login with username and password from secret file
-gc = gspread.login(secret.GD_LOGIN, secret.GD_PASS)
-points_results = gc.open("Fall 2013 House Points (Responses)")
-
-results_ws = points_results.worksheet("Form Responses")
-
-all_points = results_ws.get_all_values()
+import requests
+import csv
 
 gryffindor = 0
 ravenclaw = 0
@@ -16,8 +8,29 @@ hufflepuff = 0
 slytherin = 0
 da = 0
 
+"""
+Link for editing sheet:
+https://docs.google.com/a/hackbrightacademy.com/spreadsheet/ccc?key=0Ahc_9pLanCZudHNOTi1fVVlfX1dRdFZkWHMtSzZwNmc&usp=sharing
+"""
+# This is for people who don't mind typing their password in clear text
+# login with username and password from secret file
+"""
+gc = gspread.login(secret.GD_LOGIN, secret.GD_PASS)
+points_results = gc.open("Fall 2013 House Points (Responses)")
+results_ws = points_results.worksheet("Form Responses")
+all_points = results_ws.get_all_values()
 del all_points[0]
-for row in all_points:
+"""
+
+#Trying again without the password
+csv_request = requests.get("https://docs.google.com/spreadsheet/ccc?key=0Ahc_9pLanCZudHNOTi1fVVlfX1dRdFZkWHMtSzZwNmc&output=csv")
+csv_file = csv_request.content
+lines = csv_file.split('\n')
+del lines[0]
+for row in lines:
+    row = row.split(',')
+
+# for row in all_points:
     if row[1] == "Gryffindor":
         gryffindor += int(row[2])
     if row[1] == "Slytherin":
